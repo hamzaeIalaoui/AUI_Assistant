f938935f91d2caa28476e1eb9217e8f67fad4c12
Mohamed Amine ELOUDRHIRI <M.eloudrhiri@aui.ma>
2023-03-18 19:12:18 +0100

    adding twilio api to actions

--- actions/actions.py
+++ actions/actions.py
@@ -3,6 +3,13 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
+from twilio.rest import Client
+
+account_sid = 'AC56e526ad54a0bd99d360f25e67c4e52c'
+auth_token = 'b1e29cccb1c8e92d7535a0dd18e5545f'
+client = Client(account_sid, auth_token)
+
+
 
 class ActionVan(Action):
     def name(self):
@@ -57,3 +64,12 @@ class ActionOpeningHours(Action):
         dispatcher.utter_message(text="The opening hours of different services at the university vary. Please check with the specific service or department for their opening hours.")
 
         return []
+    
+    
+message = client.messages.create(
+  from_='whatsapp:+14155238886',
+  body='Your appointment is coming up on July 21 at 3PM',
+  to='whatsapp:+212689262985'
+)
+
+print(message.sid)
